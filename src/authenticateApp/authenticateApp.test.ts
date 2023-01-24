39223b48dcf568fbfdbf4998fdd9a1b24cb05c36
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-01-24 15:45:45 +0100

    Add test for unauthenticated app

--- src/authenticateApp/authenticateApp.test.ts
+++ src/authenticateApp/authenticateApp.test.ts
@@ -4,15 +4,33 @@ import authenticateApp from "./authenticateApp.js";
 const { apiGateway, identityServer } = appNames;
 
 describe("Given the function authenticateApp", () => {
-  describe("When it receives targetApp 'api-gateway', appToAuthenticate 'identity-server' and hashToAuthenticate 'test-hash'", () => {
+  const testHash = "hash";
+
+  describe("When it receives targetApp 'api-gateway', appToAuthenticate 'identity-server' and hashToAuthenticate 'hash'", () => {
     test("Then it should return true", async () => {
+      const isAuthenticatedResult = true;
+
       const isAuthenticated = await authenticateApp(
         apiGateway,
         identityServer,
         "hash"
       );
 
-      expect(isAuthenticated).toBe(true);
+      expect(isAuthenticated).toBe(isAuthenticatedResult);
+    });
+  });
+
+  describe("When it receives targetApp 'identity-server', appToAuthenticate 'api-gateway' and hashToAuthenticate 'hash'", () => {
+    test("Then it should return false", async () => {
+      const isAuthenticatedResult = false;
+
+      const isAuthenticated = await authenticateApp(
+        identityServer,
+        apiGateway,
+        testHash
+      );
+
+      expect(isAuthenticated).toBe(isAuthenticatedResult);
     });
   });
 });
